96e8ebbe26330d18a82ce447c6aa8a700625fa17
sabapailodze04 <sabapailodze2004@gmail.com>
2025-05-24 04:18:12 -0700

    core

---
+++ script.js
@@ -0,0 +1,8 @@
+let myname = "saba";
+
+let greeting = `hello`;
+let fulltext = `${greeting} ${myname}! welcome back`;
+let textlenght = fulltext.length;
+
+console.log(fulltext);
+console.log(textlenght);
